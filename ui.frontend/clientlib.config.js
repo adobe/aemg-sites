b789772def310b7d2d881cc3b0c538e1d69bfe9a
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-02-12 16:07:45 +0530

    ui.frontend init

--- ui.frontend/clientlib.config.js
+++ ui.frontend/clientlib.config.js
@@ -15,7 +15,7 @@
  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
 const path = require('path');
-
+const { generateClibs } = require('./clientlib-generatior');
 const BUILD_DIR = path.join(__dirname, 'dist');
 const CLIENTLIB_DIR = path.join(
   __dirname,
@@ -37,57 +37,17 @@ const libsBaseConfig = {
   jsProcessor: ['default:none', 'min:none']
 };
 
-// Config for `aem-clientlib-generator`
-module.exports = {
-  context: BUILD_DIR,
-  clientLibRoot: CLIENTLIB_DIR,
-  libs: [
-    {
-      ...libsBaseConfig,
-      name: 'clientlib-dependencies',
-      categories: ['aemguidesDALP.dependencies'],
-      assets: {
-        // Copy entrypoint scripts and stylesheets into the respective ClientLib
-        // directories
-        js: {
-          cwd: 'clientlib-dependencies',
-          files: ['**/*.js'],
-          flatten: false
-        },
-        css: {
-          cwd: 'clientlib-dependencies',
-          files: ['**/*.css'],
-          flatten: false
-        }
-      }
-    },
-    {
-      ...libsBaseConfig,
-      name: 'clientlib-site',
-      categories: ['aemguidesDALP.site'],
-      dependencies: ['aemguidesDALP.dependencies'],
-      assets: {
-        // Copy entrypoint scripts and stylesheets into the respective ClientLib
-        // directories
-        js: {
-          cwd: 'clientlib-site',
-          files: ['**/*.js'],
-          flatten: false
-        },
-        css: {
-          cwd: 'clientlib-site',
-          files: ['**/*.css'],
-          flatten: false
-        },
+const clientlibArr = [
+  {name : 'common', module: 'common'},
+  {name : 'automotive-footer', module: 'automotive'},
+]
 
-        // Copy all other files into the `resources` ClientLib directory
-        resources: {
-          cwd: 'clientlib-site',
-          files: ['**/*.*'],
-          flatten: false,
-          ignore: ['**/*.js', '**/*.css']
-        }
-      }
-    }
-  ]
-};
+const libsArr = []
+generateClibs(libsArr, libsBaseConfig, ...clientlibArr)
+
+ // Config for `aem-clientlib-generator`
+ module.exports = {
+   context: BUILD_DIR,
+   clientLibRoot: CLIENTLIB_DIR,
+   libs: libsArr
+ };
