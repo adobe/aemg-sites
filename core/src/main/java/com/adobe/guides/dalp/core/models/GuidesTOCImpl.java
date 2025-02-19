088e5f6e12f52170a8a6895196cac2524121d3de
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-02-19 15:49:02 +0530

    separate files for each dita elements

--- core/src/main/java/com/adobe/guides/dalp/core/models/GuidesTOCImpl.java
+++ core/src/main/java/com/adobe/guides/dalp/core/models/GuidesTOCImpl.java
@@ -3,9 +3,12 @@ package com.adobe.guides.dalp.core.models;
 import com.adobe.cq.export.json.ComponentExporter;
 import com.adobe.cq.wcm.core.components.util.AbstractComponentImpl;
 import com.day.cq.wcm.api.Page;
+import com.drew.lang.annotations.NotNull;
+import com.drew.lang.annotations.Nullable;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
+import org.apache.commons.codec.CharEncoding;
 import org.apache.commons.io.IOUtils;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
@@ -43,123 +46,70 @@ public class GuidesTOCImpl extends AbstractComponentImpl implements GuidesTOC {
 
     protected static final String RESOURCE_TYPE_V1 = "aemguidesDALP/components/sites-components/guides-toc";
     private static final Logger logger = LoggerFactory.getLogger(GuidesTOCImpl.class);
+
     @Self
     private SlingHttpServletRequest request;
+
     @ScriptVariable
     private Resource resource;
+
     @ScriptVariable
     private Page currentPage;
-    private String templateName;
-    private List<String> guidesNavigation;
-    private List<String> guidesNavigation2;
-    private String currentPageIndexInToc;
-    private String toString;
 
-    public GuidesTOCImpl() {
-    }
+    @Nullable
+    private String templateName;
+
+    private List<String> guidesNavigation;
+
+    private String currentPageIndexInToc;
 
     @PostConstruct
     private void initModel() {
         try {
-            Session session = (Session) this.request.getResourceResolver().adaptTo(Session.class);
-            String sitePath = (String) this.currentPage.getContentResource().getValueMap().get("sitePath", String.class);
+            Session session = request.getResourceResolver().adaptTo(Session.class);
+            String sitePath = currentPage.getContentResource().getValueMap().get("sitePath", String.class);
             logger.info("AEMSITE: sitePath: {}", sitePath);
-            Node node = session.getNode(sitePath + "/jcr:content");
+            Node node = session.getNode( sitePath + "/jcr:content");
             System.out.println(node.getPath());
             logger.info("AEMSITE: nodePath: {}", node.getPath());
+
             Binary tocBinary = node.getProperty("guides-navigation").getBinary();
             Binary tocIndexBinary = node.getProperty("guides-navigation-index").getBinary();
-            String tocBinaryString = IOUtils.toString(tocBinary.getStream(), "UTF-8");
-            String tocIndexBinaryString = IOUtils.toString(tocIndexBinary.getStream(), "UTF-8");
+            String tocBinaryString = IOUtils.toString(tocBinary.getStream(), CharEncoding.UTF_8);
+            String tocIndexBinaryString = IOUtils.toString(tocIndexBinary.getStream(), CharEncoding.UTF_8);
+
             logger.info("AEMSITE: tocBinaryString: {}", tocBinaryString);
-            JsonObject finalPathObj = new JsonObject();
-            JsonObject pathObj = new Gson().fromJson(tocBinaryString, JsonObject.class);
-            String outputPath = pathObj.get("outputPath").getAsString();
-
-            JsonArray childArr = pathObj.get("children").getAsJsonArray();
-            finalPathObj.addProperty("displayName", pathObj.get("displayName").getAsString());
-            finalPathObj.addProperty("outputPath", outputPath);
-            if (childArr.size() > 0) {
-                finalPathObj.add("children", getChildrenPath(session, childArr));
-            } else {
-                finalPathObj.add("children", new JsonArray());
-            }
-            finalPathObj.addProperty("active", true);
-            finalPathObj.addProperty("visible", true);
-
             logger.info("AEMSITE: tocIndexBinaryString: {}", tocIndexBinaryString);
+            // convert tocIndexBinaryString to JSON
             JSONObject tocIndexJson = new JSONObject(tocIndexBinaryString);
-
             try {
-                this.currentPageIndexInToc = tocIndexJson.getString(this.currentPage.getPath());
-            } catch (Exception var10) {
-                logger.warn("AEMSITE: warning: didnt find {} in tocIndexJson", this.currentPage.getPath());
-                this.currentPageIndexInToc = "0";
+                currentPageIndexInToc = tocIndexJson.getString(currentPage.getPath());
+            } catch (Exception e) {
+                logger.warn("AEMSITE: warning: didnt find {} in tocIndexJson", currentPage.getPath());
+                currentPageIndexInToc = "0";
             }
-
-            this.guidesNavigation2 = new ArrayList();
-            this.guidesNavigation2.add(finalPathObj.toString());
-        } catch (Exception var11) {
-            System.out.println("Error: " + var11.getMessage());
-            logger.error("AEMSITE: Error: {}", var11.getMessage());
+            guidesNavigation = new ArrayList<>();
+            guidesNavigation.add(tocBinaryString);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            logger.error("AEMSITE: Error: {}", e.getMessage());
         }
-
-    }
-
-    JsonArray getChildrenPath(Session session, JsonArray childrenArr) {
-        JsonArray chArr = new JsonArray();
-        childrenArr.forEach(eachObj -> {
-            try {
-                JsonObject childrenObj = eachObj.getAsJsonObject();
-                String outputPath = childrenObj.get("outputPath").getAsString();
-                JsonArray childArr = childrenObj.get("children").getAsJsonArray();
-                JsonObject obj = new JsonObject();
-                if (session.itemExists(outputPath)) {
-                    obj.addProperty("displayName", childrenObj.get("displayName").getAsString());
-                    obj.addProperty("outputPath", outputPath);
-                    if (childArr.size() > 0) {
-                        obj.add("children", getChildrenPath(session, childArr));
-                    } else {
-                        obj.add("children", new JsonArray());
-                    }
-                    obj.addProperty("active", true);
-                    obj.addProperty("visible", true);
-                    chArr.add(obj);
-                }
-
-            } catch (RepositoryException e) {
-                e.printStackTrace();
-            }
-        });
-
-        return chArr;
     }
 
     @Override
     public List<String> getGuidesNavigation() {
-        return this.guidesNavigation;
+        return guidesNavigation;
     }
 
     @Override
     public String getCurrentPageTocIndex() {
-        return this.currentPageIndexInToc;
+        return currentPageIndexInToc;
     }
 
-
+    @NotNull
     @Override
-    public String toString() {
-        return "GuidesNavigation2Impl{" +
-                "request=" + request +
-                ", resource=" + resource +
-                ", currentPage=" + currentPage +
-                ", templateName='" + templateName + '\'' +
-                ", guidesNavigation=" + guidesNavigation +
-                ", currentPageIndexInToc='" + currentPageIndexInToc + '\'' +
-                '}';
-    }
-
-    public  List<String> getGuidesNavigation2(){
-        return this.guidesNavigation2;
+    public String getExportedType() {
+        return resource.getResourceType();
     }
 
 }
