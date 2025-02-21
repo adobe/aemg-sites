718651851b965b49f709d829cdc2dc4f4f68d57f
Pratiksha Dwivedi <pratiksha.dwivedi@teknopoint.local>
2025-02-21 16:00:10 +0530

    mobile view of topic body

--- ui.frontend/src/main/webpack/clientlibs/common/js/guides-toc-search.js
+++ ui.frontend/src/main/webpack/clientlibs/common/js/guides-toc-search.js
@@ -70,53 +70,48 @@ function guidesTOCSearch(searchInput="toc-search", resultContainer="toc-search-r
 export default guidesTOCSearch;
 
 // left arrow 
-var circle = document.querySelector(".arrow-btn-left");
+var arrowbtnleft = document.querySelector(".arrow-btn-left");
 var sidepanel = document.querySelector(".left-container");
 
 
-circle.addEventListener("click", function () {
+arrowbtnleft.addEventListener("click", function () {
 
     let spWidth = sidepanel.offsetWidth;
     let spMarginLeft = parseInt(window.getComputedStyle(sidepanel).marginLeft, 10);
     let w = (spMarginLeft >= 0) ? spWidth * -1 : 0;
-    // let cw = (w < 0) ? -w : spWidth - 22;
 
     if(w < 0) {
       sidepanel.classList.add('hide-side-panel');
+      arrowbtnleft.classList.add('arrow-opposite');
     } else {
       sidepanel.classList.remove('hide-side-panel');
+      arrowbtnleft.classList.remove('arrow-opposite');
     }
 
     sidepanel.style.transition = "margin-left 0.4s ease-in-out";
-    sidepanel.style.marginLeft = w + "px";
-
-    // sidepanel.classList.add('hide-side-panel');
-
-
-    
+    sidepanel.style.marginLeft = w + "px";    
 });
 
 //right arrow 
 
-var circleRight = document.querySelector(".arrow-btn-right");
+var arrowbtnright = document.querySelector(".arrow-btn-right");
 var sidepanelRight = document.querySelector(".right-container");
 
-circleRight.addEventListener("click", function () {
+arrowbtnright.addEventListener("click", function () {
   let spWidth = sidepanelRight.offsetWidth;
   let spMarginRight = parseInt(window.getComputedStyle(sidepanelRight).marginRight, 10);
   let w = (spMarginRight >= 0) ? spWidth * -1 : 0;
 
   if(w < 0) {
     sidepanelRight.classList.add('hide-side-panel');
+    arrowbtnright.classList.add('arrow-opposite');
   } else {
     sidepanelRight.classList.remove('hide-side-panel');
+    arrowbtnright.classList.remove('arrow-opposite');
   }
-
-  // let cw = (w < 0) ? -w : spWidth - 22;
 
   sidepanelRight.style.transition = "margin-right 0.4s ease-in-out";
   sidepanelRight.style.marginRight = w + "px";
-  // sidepanelRight.classList.add('hide-side-panel');  
 });
 
 
