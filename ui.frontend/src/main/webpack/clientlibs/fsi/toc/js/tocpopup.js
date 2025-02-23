67ed0832e2de985f92676fbcb04b2534a1a51c4d
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-02-23 17:54:11 +0530

    dark mode themeing and minor fixes

--- ui.frontend/src/main/webpack/clientlibs/fsi/toc/js/tocpopup.js
+++ ui.frontend/src/main/webpack/clientlibs/fsi/toc/js/tocpopup.js
@@ -1,16 +1,20 @@
 window.addEventListener("DOMContentLoaded", function () {
   const tocPopupInit = document.querySelector(".toc-popup-init");
 
-  tocPopupInit.addEventListener("click", function () {
-    document.querySelector("body").classList.add("toc-modal");
+  // tocPopupInit.addEventListener("click", function () {
+  //   document.querySelector("body").classList.add("toc-modal");
+  // });
 
-    document.querySelector("body.toc-modal").addEventListener("click", function (e) {
+  document.querySelector("body").addEventListener("click", function (e) {
 
-      if (e.target.closest(".toc-popup-init")) return;
+    if (e.target.closest("body:not(.toc-modal) .toc-popup-init")) {
+      document.querySelector("body").classList.add("toc-modal");
+      return;
+    }
 
-      if (!e.target.closest(".cmp-guides-navigation")) {
-        document.querySelector("body").classList.remove("toc-modal");
-      }
-    });
+    if (!e.target.closest(".cmp-guides-navigation")) {
+      document.querySelector("body").classList.remove("toc-modal");
+    }
   });
+
 });
