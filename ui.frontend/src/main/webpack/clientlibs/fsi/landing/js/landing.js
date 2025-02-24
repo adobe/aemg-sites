566615ea7f25511c01c79bf1ac3a96520474060e
prashant.singh <Prashant.Singh@teknopoint.local>
2025-02-24 13:42:15 +0530

    js and css chnages landing list

--- ui.frontend/src/main/webpack/clientlibs/fsi/landing/js/landing.js
+++ ui.frontend/src/main/webpack/clientlibs/fsi/landing/js/landing.js
@@ -5,11 +5,26 @@ const listItems = document.querySelectorAll(".bfsi-list-cont .cmp-list .cmp-list
 listItems.forEach(item => {
     // Create a new button element
     const button = document.createElement("button");
-    
+
+    let listTeaserImage = item.querySelector('img');
+
+    // Check if there is no image tag in the item
+    if (!listTeaserImage) {
+        // If no image, create a new img tag
+        listTeaserImage = document.createElement('img');
+        listTeaserImage.classList.add("cmp-teaser__image");
+
+        // Optional: Set the image source here
+        listTeaserImage.src = "/content/dam/aemguidesDALP/sites-assets/fsi/leave.png";
+
+        // Append the new image to the item
+        item.appendChild(listTeaserImage);
+    }
+
     // Set the button's text or attributes
     button.textContent = "Know More";
-    
-    // Optionally, you can add some classes or attributes to style the button
+
+    // Add a class to style the button
     button.classList.add("cmp-teaser__action-container");
 
     // Append the button to the list item
