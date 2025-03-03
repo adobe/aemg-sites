8a76888154f9805261da11c1f71a4b5d48f4123f
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-03-03 16:20:26 +0530

    automotive filter impl

--- ui.frontend/clientlib.config.js
+++ ui.frontend/clientlib.config.js
@@ -51,6 +51,7 @@ const clientlibArr = [
   {name : 'automotive_topic-body', module: 'automotive'},
   {name : 'automative_toc-banner', module: 'automotive'},
   {name: 'automotive_landing', module: 'automotive'},
+  {name: 'automotive_filter', module: 'automotive'},
 
   //fsi
   {name : 'fsi_topic-body' , module: 'fsi'},
