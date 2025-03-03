446c661c738f9a92bfbee44bf04bd96d5614b1f0
pashtepooja18 <poojapashte18@gmail.com>
2025-03-03 16:43:18 +0530

    pdf functionality added and bugs resolved

--- ui.frontend/src/main/webpack/clientlibs/common/js/pdfdownload.js
+++ ui.frontend/src/main/webpack/clientlibs/common/js/pdfdownload.js
@@ -1,13 +1,57 @@
 const pdfAbsPath = "/content/dam/fmdita-outputs/pdfs/";
 
-window.addEventListener("DOMContentLoaded", function() {
+window.addEventListener("DOMContentLoaded", function () {
+
   const downloadBtn = document.querySelector(".pdf-download .cmp-button");
-  downloadBtn.addEventListener("click", function() {
-    window.open(getPDFUrl(), "_blank");
+  const ctaContainer = document.querySelector(".pdf-download");
+
+  downloadBtn.addEventListener("click", function () {
+    const pdfUrl = getPDFUrl();
+    // Check if the PDF file exists before opening it
+    checkPDFExistence(pdfUrl).then(exists => {
+      if (exists) {
+        window.open(pdfUrl, "_blank");
+      } else {
+        showNoteMessage(ctaContainer);
+      }
+    }).catch(error => {
+      console.error("Error checking PDF existence:", error);
+    });
   });
 });
 
 function getPDFUrl() {
-  const topicTitle = document.querySelector("#topic-title .cmp-title__text")?.textContent.split(" ").join("_");
-  return pdfAbsPath + topicTitle + ".pdf";
+  const topicTitle = document.querySelector("#topic-title .cmp-title__text")?.textContent;
+  if (!topicTitle) {
+    console.error("Topic title not found");
+    return "";
+  }
+  return pdfAbsPath + topicTitle.split(" ").join("_") + ".pdf";
+}
+
+function checkPDFExistence(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url, { method: 'HEAD' })
+      .then(response => {
+        if (response.ok) {
+          resolve(true); // PDF exists
+        } else {
+          resolve(false); // PDF does not exist
+        }
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+}
+function showNoteMessage(container) {
+
+  const noteMessage = document.createElement('div');
+
+  noteMessage.classList.add('pdf-note');
+  noteMessage.textContent = "Sorry, the PDF file is not available.";
+
+  if (!container.querySelector('.pdf-note')) {
+    container.appendChild(noteMessage);
+  }
 }
