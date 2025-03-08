e955060b6b1f3c41b50aae5bd9360caca0d8f8a1
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-03-08 16:31:43 +0530

    accessibility impl

--- ui.frontend/src/main/webpack/clientlibs/accessibility/js/bigcursor.js
+++ ui.frontend/src/main/webpack/clientlibs/accessibility/js/bigcursor.js
@@ -4,5 +4,5 @@ const bigCursorBtn = document.getElementById('bigcursor-btn');
 // Add an event listener to the button
 bigCursorBtn.addEventListener('click', function() {
     // Toggle the 'big-cursor-active' class on the <html> element
-    document.documentElement.classList.toggle('big-cursor-active');
+    document.body.classList.toggle('big-cursor-active');
 });
