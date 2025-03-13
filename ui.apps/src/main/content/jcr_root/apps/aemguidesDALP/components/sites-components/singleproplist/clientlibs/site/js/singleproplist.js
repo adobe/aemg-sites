32182eb5bcff9e733b24e8390c4aa8f8bc204c27
Krishna Mulik <krishna.mulik@teknopoint.local>
2025-03-13 18:33:49 +0530

    no filter ui-handle

--- ui.apps/src/main/content/jcr_root/apps/aemguidesDALP/components/sites-components/singleproplist/clientlibs/site/js/singleproplist.js
+++ ui.apps/src/main/content/jcr_root/apps/aemguidesDALP/components/sites-components/singleproplist/clientlibs/site/js/singleproplist.js
@@ -5,6 +5,7 @@ window.addEventListener("DOMContentLoaded", function () {
 
 function callafunction() {
   const ulElement = document.querySelectorAll(".singleproplist-ul");
+  let isPropsExist = false;
 
   for (let i = 0; i < ulElement.length; i++) {
     // Retrieve the data-cmp-uniqueprops attribute and parse it as JSON
@@ -25,6 +26,9 @@ function callafunction() {
         li.dataset.value = item;
         li.textContent = item;
         fragment.appendChild(li);
+        if(stringKey == "tp_vehicletype" || stringKey == "tp_carmodel") {
+          isPropsExist = true;
+        }
       }
     });
     
@@ -39,4 +43,14 @@ function callafunction() {
     // Append the fragment containing the <li> elements to the <ul>
     ulElement[i].appendChild(fragment);
   }
-}
+  
+  const filterContainer = document.querySelector(".vehicles_type");
+  if(!isPropsExist && filterContainer) {
+    filterContainer.style.display = "none";
+  }
+
+  const mobFilterBtn = document.querySelector(".vehicle_search_btn");
+  if(!isPropsExist && mobFilterBtn) {
+    mobFilterBtn.style.display = "none";
+  }
+ }
